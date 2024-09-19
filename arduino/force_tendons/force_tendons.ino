3bd8614a96658bca36e117fdbf57fe8db83be10e
Daniel Gomez <dagv21@live.com>
2024-09-19 11:28:16 +0200

    Update force_tendons.ino

--- arduino/force_tendons/force_tendons.ino
+++ arduino/force_tendons/force_tendons.ino
@@ -1,48 +1,48 @@
 //
-//    FILE: HX_read_median.ino
-//  AUTHOR: Rob Tillaart
-// PURPOSE: HX711 demo
-//     URL: https://github.com/RobTillaart/HX711
+//    FILE: force_tendons.ino
+//  AUTHOR: Daniel Gomez - Rob Tillaart (original)
+// PURPOSE: HX711 for Exoskeleton Tendons
+//     URL: https://github.com/dagv21/ankle_exoskeleton.git - https://github.com/RobTillaart/HX711
+// LIBRARY: 0.5.0
 
 
 #include "HX711.h"
 
 HX711 scale, scale1;
 
-uint8_t dataPin1 = 6;
-uint8_t clockPin1 = 7;
-uint8_t dataPin2 = 3;
-uint8_t clockPin2 = 2;
+// Posterior Sensor PINs
+uint8_t dataPin1 = 11;
+uint8_t clockPin1 = 10;
 
-uint32_t start, stop;
-volatile float f1, f2;
+// Frontal Sensor PINs
+uint8_t dataPin2 = 12;
+uint8_t clockPin2 = 13;
 
+volatile float f_frontal, f_posterior;
+
+double offset_frontal = 19005;
+double offset_posterior = 51400;
 
 void setup()
 {
   Serial.begin(250000);
 
-  scale.begin(dataPin1, clockPin1);
-  scale1.begin(dataPin2, clockPin2);
-
-  // TODO find a nice solution for this calibration..
-  // load cell factor 20 KG
-  scale.set_scale(127.15);       // TODO you need to calibrate this yourself.
-  scale1.set_scale(127.15);       // TODO you need to calibrate this yourself.
-
-  // load cell factor 5 KG
-  // scale.set_scale(420.0983);
-  // reset the scale to zero = 0
+  scale1.begin(dataPin2, clockPin2, 1);
+  scale.begin(dataPin1, clockPin1, 1);
+  
   scale.tare();
   scale1.tare();
+
+  offset_frontal = scale1.read_medavg(15);
+  offset_posterior = scale.read_medavg(15);  
 }
 
 
 void loop()
 {
-  f1 = scale.read();
-  f2 = scale1.read();
-  Serial.print(f1);
+  f_posterior = -(scale.read() - offset_posterior)*15.0/124000.0;
+  f_frontal = -(scale1.read() - offset_frontal)*15.0/182000.0;
+  Serial.print(f_frontal);
   Serial.print(",");
-  Serial.println(f2);
+  Serial.println(f_posterior);
 }
